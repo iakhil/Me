b913ea64bfb36cee4de6c2cb692358c26f8de762
I Akhil <52360071+iakhil@users.noreply.github.com>
2021-03-28 14:49:58 +0530

    Create port.js

---
+++ port.js
@@ -0,0 +1,25 @@
+function blog()
+{
+  window.open("https://www.mildlycurious.com/")
+}
+
+function about()
+{
+  window.open("about.html")
+}
+
+function contact()
+{
+  window.open("contact.html")
+}
+
+
+function highlight(btnID)
+{
+  document.getElementById(btnID).style.backgroundColor = "#f5da2f";
+}
+
+function dehighlight(btnID)
+{
+  document.getElementById(btnID).style.backgroundColor = "#a8f0d9";
+} 
